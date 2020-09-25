122f41d283af228072d883ed99573c3b9f33e09a
Ian Bryant <destroyrofevil@gmail.com>
2020-09-25 14:48:11 -0400

    dict and binary version of output.txt in decompressor

--- decompressor.py
+++ decompressor.py
@@ -1,7 +1,10 @@
+from bitarray import bitarray
 from io import StringIO
 
-wholeFileString = open('output.txt', 'rb').read()
-print(wholeFileString)
+File = open('output.txt', 'rb').read()
+#print(File)
+
+#print("byte 0",File[0])
 
 import ast, json
 
@@ -14,5 +17,22 @@ Dictionary = dict(map(reversed, originalDictionary.items()))
 for index in Dictionary:
     print(index,Dictionary[index])
 
+def access_bit(data, num):
+    base = int(num // 8)
+    shift = int(num % 8)
+    return (data[base] & (1<<shift)) >> shift
+
+
+#print(access_bit(File,-21))
+
 #for byte in wholeFileString
 
+fileBinaryString = ''
+#print("byte 1",File[1])
+#print(bin(File[1])[2:])
+leftPointer = 0
+rightPointer = 1
+bytesList = bytearray(File)
+for index in File:
+    fileBinaryString = fileBinaryString + bin(File[index])[2:]
+#print(fileBinaryString)
