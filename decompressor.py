53493059c5f70643d1c1b91148e36a97c8dec0e3
Ian Bryant <destroyrofevil@gmail.com>
2020-09-25 17:00:02 -0400

    dab

--- decompressor.py
+++ decompressor.py
@@ -1,8 +1,11 @@
 from bitarray import bitarray
 from io import StringIO
+zero = '0'
 
-File = open('output.txt', 'rb').read()
-#print(File)
+File = open('testoutput.txt', 'rb').read()
+print(File)
+print(bin(File[0]))
+print(zero * (8 - len(bin(File[1])[2:])) + bin(File[1])[2:])
 
 #print("byte 0",File[0])
 
@@ -12,7 +15,26 @@ with open('dict.json', 'r') as f:
     data = f.read()
 
 originalDictionary = json.loads(data)
+
+#finds short and long character lengths:
+holder = 0
+happened = False
+lShortChar = 0
+lLongChar = 0
+for item in originalDictionary:
+    length = len(originalDictionary[item])
+    if length > holder:
+        holder = length
+        if happened:
+            lLongChar = length
+        else:
+            lShortChar = length
+        happened = True
+
+
+#reverses dictionary:
 Dictionary = dict(map(reversed, originalDictionary.items()))
+
 
 for index in Dictionary:
     print(index,Dictionary[index])
@@ -23,16 +45,44 @@ def access_bit(data, num):
     return (data[base] & (1<<shift)) >> shift
 
 
-#print(access_bit(File,-21))
 
-#for byte in wholeFileString
-
+zero = '0'
 fileBinaryString = ''
-#print("byte 1",File[1])
-#print(bin(File[1])[2:])
 leftPointer = 0
 rightPointer = 1
 bytesList = bytearray(File)
-for index in File:
-    fileBinaryString = fileBinaryString + bin(File[index])[2:]
-#print(fileBinaryString)
+import os
+for index in range(os.stat('testoutput.txt').st_size):
+    fileBinaryString = fileBinaryString + zero * (8 - len(bin(File[index])[2:])) + bin(File[index])[2:]                         #fileBinaryString
+print(fileBinaryString)
+
+
+
+def decompress(fS,dict):
+    leftPointer = 0
+    rightPointer = 0
+    outputString = ''
+    
+   # if rightPointer >= len(fS) or leftPointer >= len(fS):
+  #      break
+
+    while 1:
+
+        if leftPointer + lShortChar > len(fS):
+            break
+
+        if fS[leftPointer] == '0':
+            rightPointer = leftPointer + lShortChar
+            outputString = outputString + dict[fS[leftPointer:rightPointer]]
+            leftPointer = rightPointer
+
+        if fS[leftPointer] == '1':
+            rightPointer = leftPointer + lLongChar
+            outputString = outputString + dict[fS[leftPointer:rightPointer]]
+            leftPointer = rightPointer
+
+
+    return outputString
+
+print(decompress(fileBinaryString,Dictionary))
+
