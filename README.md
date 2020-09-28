962f55b7e6a4a546e768dc21d23b203f3b65008b
Ian Bryant <destroyrofevil@gmail.com>
2020-09-28 18:01:20 -0400

    Cleaned the repo

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Text_Compressor
 Compresses text
 
-compressor.py generates a compression dictionary for poop.txt and compresses it into output.txt. 
+compressor.py compresses a text file using a frequency dictionary unique to that file. The dictionary elements are placed at the beginning of the compressed file. 
 
-decompressor.py decompresses output.txt into poopdecomp.txt using the dict.json file.
+decompressor.py unpacks the dictionary at the beginning of a compressed file and uses it to decompress the rest of the file.
+
+Usage:
+
+>python compressor.py [target file name] [output file name]        #Outputs a compressed version of the target file
+
+>python decompressor.py [target file name] [output file name]      #Outputs a decompressed version of the target file
--- decompressor.py
+++ decompressor.py
@@ -1,65 +1,83 @@
 from bitarray import bitarray
 from io import StringIO
+import sys
 zero = '0'
+one = '1'
 
-File = open('testoutput.txt', 'rb').read()
-print(File)
-print(bin(File[0]))
-print(zero * (8 - len(bin(File[1])[2:])) + bin(File[1])[2:])
+argsList = sys.argv         # sys.argv[1] is the target compressed file
 
-#print("byte 0",File[0])
+#build frequencyArray from start of compressed file and set position to the starting bit index of the compressed portion of the file data:
+frequencyArray = []
+position = 1
+with open(sys.argv[1],errors='ignore') as f:
+    while 1:
+        container = f.read(1)
+        position += 1
+        if container == '§':
+            break
+        frequencyArray.append(container)
+position = (position * 8)
 
-import ast, json
 
-with open('dict.json', 'r') as f: 
-    data = f.read()
+#Determine long and short bitlengths:
+requiredCharacters = len(frequencyArray)
+print(requiredCharacters)
 
-originalDictionary = json.loads(data)
-
-#find short and long character lengths:
-holder = 0
-happened = False
-lShortChar = 0
-lLongChar = 0
-for item in originalDictionary:
-    length = len(originalDictionary[item])
-    if length > holder:
-        holder = length
-        if happened:
-            lLongChar = length
+def gp2(target):
+    i = 0
+    while i > -1:
+        if pow(2,i) > target:
+            break
         else:
-            lShortChar = length
-        happened = True
+            i += 1
+    return i
+
+longCharBitLength = gp2(requiredCharacters)
+print("longCharBitLength: ",longCharBitLength)
+requiredCharacters -= pow(2,longCharBitLength-1)
+print(requiredCharacters)
+shortCharBitLength = gp2(requiredCharacters) + 1
+print("shortCharBitLength: ",shortCharBitLength)
+longCharPossibleValues = pow(2,longCharBitLength-1)
+shortCharPossibleValues = pow(2,shortCharBitLength-1)
+
+print((pow(2,longCharBitLength-1),pow(2,shortCharBitLength-1)))
+
+#Build decompression dictionary:
+def DtB(n):                                                     #Decimal to binary
+    return bin(n).replace("0b", "")
+
+Dictionary = {}
+for index in range(len(frequencyArray)):
+
+    if index < shortCharPossibleValues:
+        lz = zero * (shortCharBitLength - len(DtB(index)))
+        Dictionary[frequencyArray[index]] = lz + DtB(index)
+    else:
+        lz = one + (zero * (longCharBitLength - (len(DtB(index-shortCharPossibleValues)) + 1)))
+        Dictionary[frequencyArray[index]] = lz + DtB(index-shortCharPossibleValues)
+#Reverse dictionary
+Dictionary = dict(map(reversed, Dictionary.items()))
+for item in Dictionary:
+    print(item,Dictionary[item])
 
 
-#reverse dictionary:
-Dictionary = dict(map(reversed, originalDictionary.items()))
 
 
-for index in Dictionary:
-    print(index,Dictionary[index])
-
-def access_bit(data, num):
-    base = int(num // 8)
-    shift = int(num % 8)
-    return (data[base] & (1<<shift)) >> shift
-
-
-
-zero = '0'
+#Open file in read byte mode:
+File = open(sys.argv[1], 'rb').read()
 fileBinaryString = ''
-leftPointer = 0
-rightPointer = 1
-bytesList = bytearray(File)
+
+#Fill fileBinaryString with file data:
 import os
-for index in range(os.stat('testoutput.txt').st_size):
+for index in range(os.stat(sys.argv[1]).st_size):
     fileBinaryString = fileBinaryString + zero * (8 - len(bin(File[index])[2:])) + bin(File[index])[2:]                         #fileBinaryString
 print(fileBinaryString)
 
 
 
 def decompress(fS,dict):
-    leftPointer = 0
+    leftPointer = position
     rightPointer = 0
     outputString = ''
     
@@ -68,24 +86,24 @@ def decompress(fS,dict):
 
     while 1:
 
-        if leftPointer + lShortChar > len(fS):
+        if leftPointer + shortCharBitLength >= len(fS):
             break
 
         if fS[leftPointer] == '0':
-            rightPointer = leftPointer + lShortChar
+            rightPointer = leftPointer + shortCharBitLength
             outputString = outputString + dict[fS[leftPointer:rightPointer]]
             leftPointer = rightPointer
 
-        if fS[leftPointer] == '1':
-            rightPointer = leftPointer + lLongChar
+        elif fS[leftPointer] == '1':
+            rightPointer = leftPointer + longCharBitLength
             outputString = outputString + dict[fS[leftPointer:rightPointer]]
             leftPointer = rightPointer
 
 
     return outputString
 
-print(decompress(fileBinaryString,Dictionary))
+decomp = decompress(fileBinaryString,Dictionary)
+print(decomp)
 
-decompOutput = open('poopdecomp.txt','w')
-decompOutput.write(decompress(fileBinaryString,Dictionary))
-
+decompOutput = open(sys.argv[2],'w')
+decompOutput.write(decomp)
