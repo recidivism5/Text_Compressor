c69b9262a03d401be6b45450e3e842681c8d6cb6
Ian Bryant <destroyrofevil@gmail.com>
2020-09-28 15:18:41 -0400

    yooooooooo

--- betterdecompressor.py
+++ betterdecompressor.py
@@ -6,14 +6,17 @@ one = '1'
 
 argsList = sys.argv         # sys.argv[1] is the target compressed file
 
-#build frequencyArray from start of compressed file:
+#build frequencyArray from start of compressed file and set position to the starting bit index of the compressed portion of the file data:
 frequencyArray = []
+position = -1
 with open('testoutput.txt',errors='ignore') as f:
     while 1:
         container = f.read(1)
+        position += 1
         if container == '§':
             break
         frequencyArray.append(container)
+position = (position * 8)
 
 
 #Determine long and short bitlengths:
@@ -57,3 +60,49 @@ for index in range(len(frequencyArray)):
 Dictionary = dict(map(reversed, Dictionary.items()))
 for item in Dictionary:
     print(item,Dictionary[item])
+
+
+
+
+#Open file in read byte mode:
+File = open('testoutput.txt', 'rb').read()
+fileBinaryString = ''
+
+#Fill fileBinaryString with file data:
+import os
+for index in range(os.stat('testoutput.txt').st_size):
+    fileBinaryString = fileBinaryString + zero * (8 - len(bin(File[index])[2:])) + bin(File[index])[2:]                         #fileBinaryString
+print(fileBinaryString)
+
+
+
+def decompress(fS,dict):
+    leftPointer = position
+    rightPointer = 0
+    outputString = ''
+    
+   # if rightPointer >= len(fS) or leftPointer >= len(fS):
+  #      break
+
+    while 1:
+
+        if leftPointer + shortCharBitLength > len(fS):
+            break
+
+        if fS[leftPointer] == '0':
+            rightPointer = leftPointer + shortCharBitLength
+            outputString = outputString + dict[fS[leftPointer:rightPointer]]
+            leftPointer = rightPointer
+
+        if fS[leftPointer] == '1':
+            rightPointer = leftPointer + longCharBitLength
+            outputString = outputString + dict[fS[leftPointer:rightPointer]]
+            leftPointer = rightPointer
+
+
+    return outputString
+
+print(decompress(fileBinaryString,Dictionary))
+
+decompOutput = open('poopdecomp.txt','w')
+decompOutput.write(decompress(fileBinaryString,Dictionary))
